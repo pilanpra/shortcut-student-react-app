f05b1918af740a08f535f29b22dc653280fb7a9d
“pilanpra” <prasad416pilankar@gmail.com>
2024-01-10 20:01:23 -0800

    scroll, yellow back update

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logoImage from '../../src/img/MenuLogo.png';
+import { Link } from 'react-scroll';
 
 const MenuBar = ({ activeSection }) => {
   return (
@@ -10,8 +11,29 @@ const MenuBar = ({ activeSection }) => {
       <nav>
         <ul>
           {/* <li className={activeSection === 'home' ? 'active' : ''}><a href="#home">Home</a></li> */}
-          <li className={activeSection === 'resume' ? 'active' : ''}><a href="#resume">Resume</a></li>
-          <li className={activeSection === 'tools' ? 'active' : ''}><a href="#tools">Tools</a></li>
+          <li className={activeSection === 'resume' ? 'active' : ''}><Link
+            activeClass="active"
+            to="resume"
+            spy={true}
+            smooth={true}
+            offset={-70} // Adjust the offset as needed
+            duration={500}
+          >
+            <a href="#resume">Resume</a>
+            </Link>
+            </li>
+          <li className={activeSection === 'tools' ? 'active' : ''}>
+          <Link
+            activeClass="active"
+            to="tools"
+            spy={true}
+            smooth={true}
+            offset={-70} // Adjust the offset as needed
+            duration={500}
+          >
+            <a href="#tools">Tools</a>
+            </Link>
+            </li>
           {/* <li className={activeSection === 'job-tracker' ? 'active' : ''}><a href="#job-tracker">Job Tracker</a></li> */}
           <li className={activeSection === 'interview-prep' ? 'active' : ''}><a href="#interview-prep">Prep</a></li>
         </ul>
